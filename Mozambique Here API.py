43c7141e8ab0d22366d2eb41ad81d2fab97ed553
jakeggreen <80248380+jakeggreen@users.noreply.github.com>
2021-03-14 15:48:46 +0000

    New Apex API for session data/match history

---
+++ Mozambique Here API.py
@@ -0,0 +1,32 @@
+import requests
+import json
+
+#setup
+
+#pull in API key from text file
+APIKey_file = open('MozHere API Key.txt', 'rt')
+APIKey = APIKey_file.read()
+
+payload = {}
+headers = {}
+platform = 'PC'
+player = ''
+
+# url = 'https://api.mozambiquehe.re/bridge?player=' + player + '&platform=' + platform + '&auth=' + APIKey + '&history=1&action=info'
+
+url = 'https://api.mozambiquehe.re/maprotation?auth=' + APIKey
+
+response = requests.request('GET', url, headers=headers, data=payload)
+
+map_rotation_data = response.json()
+
+map_name = map_rotation_data.get('current').get('map')
+start = map_rotation_data.get('current').get('readableDate_start')
+end = map_rotation_data.get('current').get('readableDate_end')
+map_time_remaining = map_rotation_data.get('current').get('remainingTimer')
+next_map_name = map_rotation_data.get('next').get('map')
+next_map_start = map_rotation_data.get('next').get('readableDate_start')
+#end setup
+
+print('Current map is ' + map_name + ', for another ' + str(map_time_remaining))
+print('Next map is ' + next_map_name + ' from ' + str(next_map_start))
