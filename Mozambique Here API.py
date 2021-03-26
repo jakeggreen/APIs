632d0b0e6befa700d82fb6525e54a07071b541b8
jakeggreen <80248380+jakeggreen@users.noreply.github.com>
2021-03-26 13:14:21 +0000

    Update Mozambique Here API.py

--- Mozambique Here API.py
+++ Mozambique Here API.py
@@ -12,24 +12,24 @@ headers = {}
 platform = 'PC'
 player = 'GrumpyEconomist'
 
-url = 'https://api.mozambiquehe.re/bridge?player=' + player + '&platform=' + platform + '&auth=' + APIKey + '&history=1&action=get'
+# url = 'https://api.mozambiquehe.re/bridge?player=' + player + '&platform=' + platform + '&auth=' + APIKey + '&history=1&action=get'
 
-# url = 'https://api.mozambiquehe.re/maprotation?auth=' + APIKey
+url = 'https://api.mozambiquehe.re/maprotation?auth=' + APIKey
 
 response = requests.request('GET', url, headers=headers, data=payload)
 
 map_rotation_data = response.json()
 
-print(map_rotation_data)
+# print(map_rotation_data)
 
 #end setup
 
-# map_name = map_rotation_data.get('current').get('map')
-# start = map_rotation_data.get('current').get('readableDate_start')
-# end = map_rotation_data.get('current').get('readableDate_end')
-# map_time_remaining = map_rotation_data.get('current').get('remainingTimer')
-# next_map_name = map_rotation_data.get('next').get('map')
-# next_map_start = map_rotation_data.get('next').get('readableDate_start')
+map_name = map_rotation_data.get('current').get('map')
+start = map_rotation_data.get('current').get('readableDate_start')
+end = map_rotation_data.get('current').get('readableDate_end')
+map_time_remaining = map_rotation_data.get('current').get('remainingTimer')
+next_map_name = map_rotation_data.get('next').get('map')
+next_map_start = map_rotation_data.get('next').get('readableDate_start')
 
-# print('Current map is ' + map_name + ', for another ' + str(map_time_remaining))
-# print('Next map is ' + next_map_name + ' from ' + str(next_map_start))
+print('Current map is ' + map_name + ', for another ' + str(map_time_remaining))
+print('Next map is ' + next_map_name + ' from ' + str(next_map_start))
